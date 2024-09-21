ff679ec742de02f5722ba3d4600c8dfa912906d5
Muhammad-Taha-Qader <m.tahaqader000@gmail.com>
2024-09-21 18:11:11 +0500

    Caesar Cipher - In dir: Basic

--- Basic/Caesar_Cipher.rb
+++ Basic/Caesar_Cipher.rb
@@ -1,6 +1,12 @@
 def caesar_cipher(str, shift) 
-    str2 = (str.split("").map{|char| p (char.ord + shift).chr})
+    str2 = (str.split("").map{ |char| 
+        next char unless (('a'..'z').include?(char) || ('A'..'Z').include?(char)) #next act as return for that itertion keepinf the map loop on
+        p (((char.ord + shift) > 'z'.ord && (char =~ /[a-z]/)) || ((char.ord + shift) > 'Z'.ord && (char =~ /[A-Z]/)))?   
+            (char.ord + shift - (26) ).chr   
+            : (char.ord + shift).chr 
+    })
     str2.join
 end
 
-p caesar_cipher("abc",3)
+p caesar_cipher("a,5xyzXY--Aaz",3)
+
